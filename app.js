27f9b992c8e0a8f9a23a3e50c5b4f824f5ba928d
skgmdz <skgmdz@gmail.com>
2016-07-26 23:14:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'EmporiDice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6LdkDSYTAAAAAGnq78t42w3MLZ1grLQUgd3DsxKy',  // <----- EDIT ME!
   redirect_uri: 'skgmdz.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
